2217c8cfc1129e2b050e682706fb2bb5435e5e11
clgoins <clgoins812@gmail.com>
2023-03-05 21:31:25 -0500

    changed project name
    Changed project name from BASIC_OSC to MIDI_OSC. Got rid of VS2019 export option in Projucer. Cleaned and regenerated all the Visual Studio stuff from the project directory.

--- JuceLibraryCode/JucePluginDefines.h
+++ JuceLibraryCode/JucePluginDefines.h
@@ -41,10 +41,10 @@
  #define JucePlugin_Enable_ARA             0
 #endif
 #ifndef  JucePlugin_Name
- #define JucePlugin_Name                   "BASIC OSC"
+ #define JucePlugin_Name                   "MIDI OSC"
 #endif
 #ifndef  JucePlugin_Desc
- #define JucePlugin_Desc                   "BASIC OSC"
+ #define JucePlugin_Desc                   "MIDI OSC"
 #endif
 #ifndef  JucePlugin_Manufacturer
  #define JucePlugin_Manufacturer           "FALCON LABS"
@@ -101,19 +101,19 @@
  #define JucePlugin_AUSubType              JucePlugin_PluginCode
 #endif
 #ifndef  JucePlugin_AUExportPrefix
- #define JucePlugin_AUExportPrefix         BASICOSCAU
+ #define JucePlugin_AUExportPrefix         MIDIOSCAU
 #endif
 #ifndef  JucePlugin_AUExportPrefixQuoted
- #define JucePlugin_AUExportPrefixQuoted   "BASICOSCAU"
+ #define JucePlugin_AUExportPrefixQuoted   "MIDIOSCAU"
 #endif
 #ifndef  JucePlugin_AUManufacturerCode
  #define JucePlugin_AUManufacturerCode     JucePlugin_ManufacturerCode
 #endif
 #ifndef  JucePlugin_CFBundleIdentifier
- #define JucePlugin_CFBundleIdentifier     com.yourcompany.BASICOSC
+ #define JucePlugin_CFBundleIdentifier     com.yourcompany.MIDIOSC
 #endif
 #ifndef  JucePlugin_AAXIdentifier
- #define JucePlugin_AAXIdentifier          com.yourcompany.BASICOSC
+ #define JucePlugin_AAXIdentifier          com.yourcompany.MIDIOSC
 #endif
 #ifndef  JucePlugin_AAXManufacturerCode
  #define JucePlugin_AAXManufacturerCode    JucePlugin_ManufacturerCode
@@ -137,7 +137,7 @@
  #define JucePlugin_IAASubType             JucePlugin_PluginCode
 #endif
 #ifndef  JucePlugin_IAAName
- #define JucePlugin_IAAName                "FALCON LABS: BASIC OSC"
+ #define JucePlugin_IAAName                "FALCON LABS: MIDI OSC"
 #endif
 #ifndef  JucePlugin_VSTNumMidiInputs
  #define JucePlugin_VSTNumMidiInputs       16
@@ -152,10 +152,10 @@
  #define JucePlugin_ARATransformationFlags  0
 #endif
 #ifndef  JucePlugin_ARAFactoryID
- #define JucePlugin_ARAFactoryID           "com.yourcompany.BASICOSC.factory"
+ #define JucePlugin_ARAFactoryID           "com.yourcompany.MIDIOSC.factory"
 #endif
 #ifndef  JucePlugin_ARADocumentArchiveID
- #define JucePlugin_ARADocumentArchiveID   "com.yourcompany.BASICOSC.aradocumentarchive.1.0.0"
+ #define JucePlugin_ARADocumentArchiveID   "com.yourcompany.MIDIOSC.aradocumentarchive.1.0.0"
 #endif
 #ifndef  JucePlugin_ARACompatibleArchiveIDs
  #define JucePlugin_ARACompatibleArchiveIDs  ""
